0a1c9ab2c4a4f1669de6de65040edfa7838eccc5
Megan McBride <megan.mcbride@colorado.edu>
2024-05-23 21:34:24 -0600

    Add guess entry form

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,14 +2,19 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+import GuessForm from '../GuessForm/GuessForm';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
+const updateWords = (currentGuess) => {
+  console.log({currentGuess})
+}
+
 function Game() {
-  return <>Put a game here!</>;
+  return <><GuessForm updateWords={updateWords}/></>;
 }
 
 export default Game;
---
+++ src/components/GuessForm/GuessForm.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+
+function GuessForm({updateWords}) {
+  const [currentGuess, setCurrentGuess] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateWords(currentGuess);
+    setCurrentGuess('');
+  }
+  return (
+  <form className="guess-input-wrapper" onSubmit={(e) => {
+handleSubmit(e)
+  }}>
+  <label htmlFor="guess-input">Venture a guess:</label>
+  <input 
+  id="guess-input" 
+  type="text" 
+  pattern="[a-zA-Z]{5}"
+  minLength={5}
+  maxLength={5}
+  title="5 letter guess"
+  value={currentGuess}
+  onChange={e => setCurrentGuess(e.target.value.toUpperCase())}
+  />
+</form>
+);
+}
+
+export default GuessForm;
