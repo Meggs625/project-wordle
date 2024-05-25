edcac3164d05a88b61def596310432ce5f8a9bb6
Megan McBride <megan.mcbride@colorado.edu>
2024-05-24 19:34:04 -0600

    Add load new game button

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,22 +5,24 @@ import { WORDS } from '../../data';
 import GuessForm from '../GuessForm/GuessForm';
 import GameBoard from '../GameBoard/GameBoard';
 import { checkGuess } from '../../game-helpers';
-// Pick a random word on every pageload.
-const answer = sample(WORDS);
-// To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
+import Confetti from 'react-confetti';
+import useWindowSize from 'react-use/lib/useWindowSize';
 
 const GAME_STATUS = {
   WON: "WON",
   LOST: "LOST"
 }
 
-
-
 function Game() {
+  const [answer, setAnswer] = useState(() => sample(WORDS))
   const [previousGuesses, setPreviousGuesses] = useState([]);
   const [gameStatus, setGameStatus] = useState('');
+  const [runConfetti, setRunConfetti] = useState(false);
+
   const numOfGuesses = useRef(0);
+
+  const { width, height } = useWindowSize()
+
 
   const handleNewWord = (currentGuess) => {
     numOfGuesses.current = numOfGuesses.current + 1;
@@ -33,6 +35,7 @@ function Game() {
     const isAWinner = currentGuess === answer;
     if (isAWinner) {
       setGameStatus(GAME_STATUS.WON);
+      setRunConfetti(true)
       return;
     } 
 
@@ -42,6 +45,15 @@ function Game() {
     }
   }
 
+  const loadNewGame = () => {
+    setAnswer(sample(WORDS));
+    setPreviousGuesses([]);
+    setGameStatus('');
+    setRunConfetti(false);
+    numOfGuesses.current = 0;
+    //could just run location.reload()? 
+  }
+
   return <>
   <GameBoard previousGuesses={previousGuesses}/>
   <GuessForm handleNewWord={handleNewWord} gameStatus={gameStatus}/>
@@ -49,14 +61,27 @@ function Game() {
     <div className="happy banner">
     <p>
       <strong>Congratulations!</strong> Got it in
-      <strong>{`${numOfGuesses.current} guesses!`}</strong>.
+      <strong>{` ${numOfGuesses.current} guesses!`}</strong>.
     </p>
+    <button className="load-new-game-btn" onClick={loadNewGame}>Play again!</button>
+
   </div>
+
   )}
   {gameStatus === GAME_STATUS.LOST && (
     <div className="sad banner">
     <p>Sorry, the correct answer is <strong>{answer}</strong>.</p>
+    <button className="load-new-game-btn" onClick={loadNewGame}>Try again?</button>
   </div>
+  )}
+  {runConfetti && (
+    <Confetti
+    width={width}
+    height={height}
+    numberOfPieces={400}
+    recycle={false}
+  />
+
   )}
   </>;
 }
